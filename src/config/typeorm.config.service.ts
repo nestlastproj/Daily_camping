138a685e4ec1696fb8812c576c09fdbe71c35efc
jo haebeen <haebin1622@naver.com>
2023-02-27 21:34:18 +0900

    feat: auth, congfig 생성

---
+++ src/config/typeorm.config.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config/dist';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm/dist';
+
+@Injectable()
+export class TypeOrmConfigService implements TypeOrmOptionsFactory {
+  constructor(private readonly configService: ConfigService) {}
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'mysql',
+      host: this.configService.get<string>('DATABASE_HOST'),
+      port: this.configService.get<number>('DATABASE_PORT'),
+      username: this.configService.get<string>('DATABASE_USERNAME'),
+      password: this.configService.get<string>('DATABASE_PASSWORD'),
+      database: this.configService.get<string>('DATABASE_NAME'),
+      entities: [],
+      synchronize: this.configService.get<boolean>('DATABASE_SYNCHRONIZE'),
+    };
+  }
+}
