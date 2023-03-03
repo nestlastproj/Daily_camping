a18c6cb6d418e2ff0276d8194402187d3033e7d9
wonbin Choi <maraineryu@gmail.com>
2023-03-03 18:27:23 +0900

    feat:Task Scheduling 예제

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
 
 @Injectable()
 export class AppService {
+  private readonly logger = new Logger(AppService.name);
   getHello(): string {
     return 'Hello World!';
+  }
+  @Cron('10 * * * * *')
+  handleCron() {
+    this.logger.debug('10초마다 불러온다');
+  }
+  @Cron(CronExpression.EVERY_30_SECONDS)
+  handleCron2() {
+    this.logger.debug('30초마다 불러온다');
   }
 }
