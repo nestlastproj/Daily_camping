9346c42782a3c9595257b28b6c0f4314803fc281
gyull92 <wjsrbfuf@gmail.com>
2023-03-12 02:37:27 +0900

    feat: 레시피 크롤링 스케줄러 적용

--- src/recipe/recipe.controller.ts
+++ src/recipe/recipe.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
+import { Cron } from '@nestjs/schedule';
 import { RecipeService } from './recipe.service';
 
 @Controller('recipe')
 export class RecipeController {
   constructor(private readonly recipeService: RecipeService) {}
 
+  // @Cron('* * * 9 * *')
   @Get('/')
   getRecipe() {
     return this.recipeService.getRecipe();
