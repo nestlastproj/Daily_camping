83d11df824fb4bbc6addf9bfda7d57a082b8a27d
jo haebeen <haebin1622@naver.com>
2023-03-23 04:00:00 +0900

    fix : 회원정보 수정 시 오류 수정

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -1,6 +1,5 @@
-import { ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { Observable } from 'rxjs';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {}
